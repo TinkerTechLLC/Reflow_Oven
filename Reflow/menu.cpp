d9e40fa2fadb403ff66782a3951f49f3f284922d
Michael Ploof <michael@dynamicperception.com>
2016-12-01 12:35:42 -0500

    Untabbifying

--- Reflow/menu.cpp
+++ Reflow/menu.cpp
@@ -6,7 +6,7 @@
 
 /************************************************
 
-	Constructors and Initialization Function
+    Constructors and Initialization Function
 
 *************************************************/
 
@@ -14,207 +14,207 @@
 // Constuctor with optional contrast setting
 Menu::Menu() {
 
-	// Initialize most of the class variables
-	init();
-	display.setContrast(35);
+    // Initialize most of the class variables
+    init();
+    display.setContrast(35);
 }
 
 Menu::Menu(int p_contrast = 35) {
 
-	// Initialize most of the class variables
-	init();
-	display.setContrast(p_contrast);
+    // Initialize most of the class variables
+    init();
+    display.setContrast(p_contrast);
 }
 
 void Menu::init(){
 
-	// Initialize vars
-	m_cursor_pos = 0;
-	m_element_selected = false;
-	m_contents = NULL;
-	m_header = "NULL";
-	m_vis_count = m_MAX_LINES;
+    // Initialize vars
+    m_cursor_pos = 0;
+    m_element_selected = false;
+    m_contents = NULL;
+    m_header = "NULL";
+    m_vis_count = m_MAX_LINES;
 
-	// Populate visible elements counting by 1 from up to total array size
-	for (byte i = 0; i < m_MAX_LINES; i++) {
-		m_vis_elements[i] = i;
-	}
-	
-	
+    // Populate visible elements counting by 1 from up to total array size
+    for (byte i = 0; i < m_MAX_LINES; i++) {
+        m_vis_elements[i] = i;
+    }
+    
+    
 }
 
 
 /************************************************
 
-			 Menu Setup Function
+             Menu Setup Function
 
 *************************************************/
 
 void Menu::begin() {
-	// Initialize display 	
-	display.begin();	
+    // Initialize display 	
+    display.begin();	
 }
 
 // Set the pointer to the array containing the menu elements
 void Menu::setContents(element(*p_contents), int p_menu_size) {
-	m_contents = p_contents;
-	m_menu_size = p_menu_size;
+    m_contents = p_contents;
+    m_menu_size = p_menu_size;
 }
 
 
 /************************************************
 
-			   Cursor Functions
+               Cursor Functions
 
 *************************************************/
 
 // Move cursor up one element or increase element value
 void Menu::up(){
-	
-	if (!m_element_selected){
-		m_cursor_pos--;
+    
+    if (!m_element_selected){
+        m_cursor_pos--;
 
-		// If the new position is below zero, loop the menu back to the end
-		if (m_cursor_pos < 0) {
-			Serial.println("Looping to bottom");
-			m_cursor_pos = m_menu_size - 1;
-			// Re-set the visible elements to the end of the list
-			for (byte i = 0; i < m_vis_count; i++) {
-				m_vis_elements[i] = m_menu_size - m_vis_count + i;
-			}
-		}
+        // If the new position is below zero, loop the menu back to the end
+        if (m_cursor_pos < 0) {
+            Serial.println("Looping to bottom");
+            m_cursor_pos = m_menu_size - 1;
+            // Re-set the visible elements to the end of the list
+            for (byte i = 0; i < m_vis_count; i++) {
+                m_vis_elements[i] = m_menu_size - m_vis_count + i;
+            }
+        }
 
-		// If this would move us off the screen, but not past the last menu element, scroll the menu contents
-		else if (m_cursor_pos < m_vis_elements[0] && m_cursor_pos >= 0){
-			Serial.println("Scrolling up");
-			for (byte i = 0; i < m_vis_count; i++) {
-				m_vis_elements[i]--;
-			}
-		}
-	}
+        // If this would move us off the screen, but not past the last menu element, scroll the menu contents
+        else if (m_cursor_pos < m_vis_elements[0] && m_cursor_pos >= 0){
+            Serial.println("Scrolling up");
+            for (byte i = 0; i < m_vis_count; i++) {
+                m_vis_elements[i]--;
+            }
+        }
+    }
 
-	// If the element is selected, increase its value by the appropriate increment, but not above the maximum
-	else if (m_element_selected && (*m_contents[m_cursor_pos].value + m_contents[m_cursor_pos].increment) <= m_contents[m_cursor_pos].max)
-		*m_contents[m_cursor_pos].value += m_contents[m_cursor_pos].increment;
+    // If the element is selected, increase its value by the appropriate increment, but not above the maximum
+    else if (m_element_selected && (*m_contents[m_cursor_pos].value + m_contents[m_cursor_pos].increment) <= m_contents[m_cursor_pos].max)
+        *m_contents[m_cursor_pos].value += m_contents[m_cursor_pos].increment;
 
-	refresh();
+    refresh();
 }
 
 // Move cursor down one element or decrease element value
 void Menu::down(){
 
-	// Move the cursor if the element is not selected
-	if (!m_element_selected) {
+    // Move the cursor if the element is not selected
+    if (!m_element_selected) {
 
-		m_cursor_pos++;
+        m_cursor_pos++;
 
-		// If the new position exceeds the menu length, loop the menu back to the start
-		if (m_cursor_pos > m_menu_size - 1) {
-			Serial.println("Looping to top");
-			m_cursor_pos = 0;
-			// Re-set the visible elements back to the beginning of the list
-			for (byte i = 0; i < m_vis_count; i++) {
-				m_vis_elements[i] = i;
-			}
-		}
+        // If the new position exceeds the menu length, loop the menu back to the start
+        if (m_cursor_pos > m_menu_size - 1) {
+            Serial.println("Looping to top");
+            m_cursor_pos = 0;
+            // Re-set the visible elements back to the beginning of the list
+            for (byte i = 0; i < m_vis_count; i++) {
+                m_vis_elements[i] = i;
+            }
+        }
 
-		// If this would move us off the screen, but not past the last menu element, scroll the menu contents
-		else if (m_cursor_pos > m_vis_elements[m_vis_count - 1] && m_cursor_pos <= m_menu_size - 1) {
-			Serial.println("Scrolling down");
-			for (byte i = 0; i < m_vis_count; i++) {
-				m_vis_elements[i]++;
-			}
-		}
-	}
+        // If this would move us off the screen, but not past the last menu element, scroll the menu contents
+        else if (m_cursor_pos > m_vis_elements[m_vis_count - 1] && m_cursor_pos <= m_menu_size - 1) {
+            Serial.println("Scrolling down");
+            for (byte i = 0; i < m_vis_count; i++) {
+                m_vis_elements[i]++;
+            }
+        }
+    }
 
-	// If the element is selected, decrease its value by the appropriate increment, but not below the minimum
-	else if (m_element_selected && (*m_contents[m_cursor_pos].value - m_contents[m_cursor_pos].increment) >= m_contents[m_cursor_pos].min)
-		*m_contents[m_cursor_pos].value -= m_contents[m_cursor_pos].increment;
+    // If the element is selected, decrease its value by the appropriate increment, but not below the minimum
+    else if (m_element_selected && (*m_contents[m_cursor_pos].value - m_contents[m_cursor_pos].increment) >= m_contents[m_cursor_pos].min)
+        *m_contents[m_cursor_pos].value -= m_contents[m_cursor_pos].increment;
 
-	refresh();
+    refresh();
 }
 
 // Toggle the current element selection state
 void Menu::select(){
-	m_element_selected = !m_element_selected;
-	refresh();
+    m_element_selected = !m_element_selected;
+    refresh();
 }
 
 
 /************************************************
 
-			  Display Functions
+              Display Functions
 
 *************************************************/
 
 // Clear the display
 void Menu::clear() {
-	display.clearDisplay();
-	display.display();
+    display.clearDisplay();
+    display.display();
 }
 
 // Set the header text. This is the line that will appear at 
 // the top of the screen and will not move when elements scroll
 void Menu::setHeader(String p_header){
-	
-	m_header = p_header;
+    
+    m_header = p_header;
 
-	// If a header is used, offset the menu to make room
-	if (m_header != "NULL")
-		m_vis_count = m_MAX_LINES - m_HEADER_OFFSET;
-	else
-		m_vis_count = m_MAX_LINES;
+    // If a header is used, offset the menu to make room
+    if (m_header != "NULL")
+        m_vis_count = m_MAX_LINES - m_HEADER_OFFSET;
+    else
+        m_vis_count = m_MAX_LINES;
 }
 
 // Print one element on a particular line
 void Menu::displayElement(int p_item, int p_line) {
 
-	// Move to the appropriate display line
-	display.setCursor(0, p_line * m_CHAR_HEIGHT);
+    // Move to the appropriate display line
+    display.setCursor(0, p_line * m_CHAR_HEIGHT);
 
-	// Print the selector icon if needed
-	if (p_item == m_cursor_pos && !m_element_selected)
-		display.print("*");
+    // Print the selector icon if needed
+    if (p_item == m_cursor_pos && !m_element_selected)
+        display.print("*");
 
-	// Move one character to the left
-	display.setCursor(1 * m_CHAR_WIDTH, p_line * m_CHAR_HEIGHT);
+    // Move one character to the left
+    display.setCursor(1 * m_CHAR_WIDTH, p_line * m_CHAR_HEIGHT);
 
-	// Print the element name
-	display.print(m_contents[p_item].name);
+    // Print the element name
+    display.print(m_contents[p_item].name);
 
-	// Print the appropriate separator, determined by whether the element is selected
-	if (p_item == m_cursor_pos && m_element_selected)
-		display.print("= ");
-	else
-		display.print(": ");
+    // Print the appropriate separator, determined by whether the element is selected
+    if (p_item == m_cursor_pos && m_element_selected)
+        display.print("= ");
+    else
+        display.print(": ");
 
-	// Print the element value and unit
-	display.print(*m_contents[p_item].value);
-	display.print(m_contents[p_item].unit);
+    // Print the element value and unit
+    display.print(*m_contents[p_item].value);
+    display.print(m_contents[p_item].unit);
 }
 
 // Redraw the screen with updated info
 void Menu::refresh() {
 
-	int header_offset = 0;
+    int header_offset = 0;
 
-	display.clearDisplay();
-	display.setTextSize(1);
-	
-	if (m_header != "NULL") {
-		display.setCursor(0, 0);
-		display.print(m_header);
-		header_offset = 2;
-	}
+    display.clearDisplay();
+    display.setTextSize(1);
+    
+    if (m_header != "NULL") {
+        display.setCursor(0, 0);
+        display.print(m_header);
+        header_offset = 2;
+    }
 
-	for (byte i = 0; i < m_vis_count; i++) {
-		Serial.print("Line: ");
-		Serial.print(i + m_HEADER_OFFSET);
-		Serial.print(" Menu item: ");
-		Serial.println(m_vis_elements[i]);
-		displayElement(m_vis_elements[i], i + m_HEADER_OFFSET);
-	}
+    for (byte i = 0; i < m_vis_count; i++) {
+        Serial.print("Line: ");
+        Serial.print(i + m_HEADER_OFFSET);
+        Serial.print(" Menu item: ");
+        Serial.println(m_vis_elements[i]);
+        displayElement(m_vis_elements[i], i + m_HEADER_OFFSET);
+    }
 
-	display.display();
+    display.display();
 
 }
--- Reflow/menu.h
+++ Reflow/menu.h
@@ -1,7 +1,7 @@
 // menu.h
 
 /*********************************************************************
-						Nokia 5110 Menu System
+                        Nokia 5110 Menu System
 
 This library supports a menu system for an arbitrary number of items.
 When selected, the user may adjust the value of the current item within
@@ -26,60 +26,58 @@ screen drawing tasks.
 class Menu {
 
 public:
-	Menu();								// Default constructor
-	Menu(int);							// Constructor with optional constrast setting
+    Menu();                             // Default constructor
+    Menu(int);                          // Constructor with optional constrast setting
 
-	void begin();
+    void begin();
 
-	struct element {					// Structure of menu elements
-		String name;
-		int *value;
-		String unit;
-		int min;
-		int max;
-		int increment;
-	};
+    struct element {                    // Structure of menu elements
+        String name;
+        int *value;
+        String unit;
+        int min;
+        int max;
+        int increment;
+    };
 
-	void setContents(element(*), int);	// Accepts a pointer to an array of elements
-	void setHeader(String);				// Sets the text to display at the top of the screen
+    void setContents(element(*), int);  // Accepts a pointer to an array of elements
+    void setHeader(String);             // Sets the text to display at the top of the screen
 
-	void up();							// Move cursor up one menu element
-	void down();						// Move cursor down one menu element
-	void select();						// Select or deselect the current menu element
+    void up();                          // Move cursor up one menu element
+    void down();                        // Move cursor down one menu element
+    void select();                      // Select or deselect the current menu element
 
-	void clear();						// Completely clear the screen
-	void refresh();						// Refresh the screen with updated menu info
+    void clear();                       // Completely clear the screen
+    void refresh();                     // Refresh the screen with updated menu info
 
-	// Software SPI (slower updates, more flexible pin options):
-	// pin 7 - Serial clock out (SCLK)
-	// pin 6 - Serial data out (DIN)
-	// pin 5 - Data/Command select (D/C)
-	// pin 4 - LCD chip select (CS)
-	// pin 3 - LCD reset (RST)
-	Adafruit_PCD8544 display;
+    // Software SPI (slower updates, more flexible pin options):
+    // pin 7 - Serial clock out (SCLK)
+    // pin 6 - Serial data out (DIN)
+    // pin 5 - Data/Command select (D/C)
+    // pin 4 - LCD chip select (CS)
+    // pin 3 - LCD reset (RST)
+    Adafruit_PCD8544 display;
 
-	int m_cursor_pos;															// Current element targeted
-	bool m_element_selected;													// Whether current element is selected for modification
+    int m_cursor_pos;                                                   // Current element targeted
+    bool m_element_selected;                                            // Whether current element is selected for modification
 
 private:
-	void init();
-	static const int m_SCREEN_HEIGHT = 48;
-	static const int m_SCREEN_WIDTH = 84;
-	static const int m_CHAR_HEIGHT = 8;
-	static const int m_CHAR_WIDTH = 6;
-	static const int m_HEADER_OFFSET = 2;
+    void init();
+    static const int m_SCREEN_HEIGHT = 48;
+    static const int m_SCREEN_WIDTH = 84;
+    static const int m_CHAR_HEIGHT = 8;
+    static const int m_CHAR_WIDTH = 6;
+    static const int m_HEADER_OFFSET = 2;
 
-	String m_header = "NULL";													// Text to display at top of the window. Ignored if m_header == "NULL"
+    String m_header = "NULL";                                            // Text to display at top of the window. Ignored if m_header == "NULL"
 
-	element(*m_contents);														// Pointer to the array of menu elements
-	int	m_menu_size;															// Number of items in the array of menu elements
-	static const int m_MAX_LINES = m_SCREEN_HEIGHT / m_CHAR_HEIGHT;				// Number of elements that can be simultaneously viewed on the screen
-	int m_vis_elements[m_MAX_LINES];											// Array of element reference numbers that are currently visible
-	unsigned int m_vis_count;
+    element(*m_contents);                                                // Pointer to the array of menu elements
+    int	m_menu_size;                                                     // Number of items in the array of menu elements
+    static const int m_MAX_LINES = m_SCREEN_HEIGHT / m_CHAR_HEIGHT;      // Number of elements that can be simultaneously viewed on the screen
+    int m_vis_elements[m_MAX_LINES];                                     // Array of element reference numbers that are currently visible
+    unsigned int m_vis_count;
 
-	void displayElement(int, int);												// Prints one element from the menu list at a specified location
-
-
+    void displayElement(int, int);                                       // Prints one element from the menu list at a specified location
 };
 
 #endif
